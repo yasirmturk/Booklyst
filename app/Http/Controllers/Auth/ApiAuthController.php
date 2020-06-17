505cb49b0b3ebcd7cc7da62965a4708b2baace77
Yasir M Turk <yasirmturk@gmail.com>
2020-06-18 02:24:57 +0700

    working on register

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -5,18 +5,76 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class ApiAuthController extends Controller
 {
+    private $client;
+
+    /**
+     * DefaultController constructor.
+     */
+    public function __construct()
+    {
+        $this->client = DB::table('oauth_clients')->where('id', 1)->first();
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    protected function authenticate(Request $request)
+    {
+        $request->request->add([
+            'grant_type' => 'password',
+            'username' => $request->email,
+            'password' => $request->password,
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'scope' => ''
+        ]);
+
+        $proxy = Request::create('oauth/token', 'POST');
+
+        return \Route::dispatch($proxy);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    protected function refreshToken(Request $request)
+    {
+        $request->request->add([
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $request->refresh_token,
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'scope' => ''
+        ]);
+
+        $proxy = Request::create(
+            'oauth/token',
+            'POST'
+        );
+
+        return \Route::dispatch($proxy);
+    }
+
     public function register (Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
             'type' => 'integer',
+            //'client_id' => ['required', Rule::exists('oauth_clients', 'id')],
+            // 'client_secret' => [
+            //     'required',
+            //     Rule::exists('oauth_clients', 'secret')->where('id', request()->get('client_id', 0))
+            // ]
         ]);
         if ($validator->fails())
         {
@@ -26,9 +84,19 @@ class ApiAuthController extends Controller
         $request['remember_token'] = Str::random(10);
         $request['type'] = $request['type'] ? $request['type']  : 0;
         $user = User::create($request->toArray());
-        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $token = $user->createToken('Postman')->accessToken;
         $response = ['token' => $token];
         return response($response, 200);
+        // $request->request->add([
+        //     'grant_type' => 'password',
+        //     'username' => $request['email'],
+        //     'password' => $request['password'],
+        //     'client_id' => $this->client->id,
+        //     'client_secret' => $this->client->secret,
+        //     'scope' => ''
+        // ]);
+        // $proxy = Request::create('oauth/token', 'POST');
+        // return \Route::dispatch($proxy);
     }
 
     public function login (Request $request) {
