e0b85fc554200be067087f10104dde0bedabcc16
Yasir M Turk <yasir.turk@agoda.com>
2020-07-01 19:46:10 +0700

    Adds vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -40,4 +43,5 @@ Vue.component(
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
 });
