5d6089b62bda5c336e20b73b16bd2352f0c67cb5
Yasir M Turk <yasir.turk@agoda.com>
2020-07-09 18:34:51 +0700

    Updates business category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
      * @var array
      */
     protected $fillable = [
-        'name'
+        'name', 'is_service', 'is_product'
     ];
 
     /**
--- resources/views/admin/settings/category.blade.php
+++ resources/views/admin/settings/category.blade.php
@@ -2,10 +2,42 @@
 @section('content')
 <!-- Main content -->
 <div class="content">
-    Category
-    @foreach ($categories as $category)
-    <p>This is user {{ $category->name }}</p>
-    @endforeach
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Category</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        @foreach ($categories as $category)
+                        <div class="form-group clearfix">
+                            <strong>{{ $category->name }}</strong>
+                            <div class="icheck-primary d-inline">
+                                <input type="checkbox" id="checkboxService" {{ $category->is_service ? 'checked':'' }}>
+                                <label for="checkboxServices">
+                                    Service
+                                </label>
+                            </div>
+                            <div class="icheck-primary d-inline">
+                                <input type="checkbox" id="checkboxProduct" {{ $category->is_product ? 'checked':'' }}>
+                                <label for="checkboxProduct">
+                                    Product
+                                </label>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
 </div>
 <!-- /.content -->
 @endsection
