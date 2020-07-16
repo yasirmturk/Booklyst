e8264eb84c04c4ae65cb327bc09a5a46ae9c393c
Yasir M Turk <yasir.turk@agoda.com>
2020-07-16 19:00:54 +0700

    Disables not used services

--- config/services.php
+++ config/services.php
@@ -20,15 +20,15 @@ return [
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+    // 'postmark' => [
+    //     'token' => env('POSTMARK_TOKEN'),
+    // ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+    // 'ses' => [
+    //     'key' => env('AWS_ACCESS_KEY_ID'),
+    //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    //     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    // ],
 
     // Socalite
     'google' => [
