a88dd3faa930bef2809c116eafc8283c28e302d8
Yasir M Turk <yasirmturk@gmail.com>
2020-11-20 00:35:43 +0100

    Builds product and service admin pages

--- resources/views/admin/service.blade.php
+++ resources/views/admin/service.blade.php
@@ -7,12 +7,23 @@
                 @foreach($services as $service)
                 <li class="list-group-item">
                     <div class="row">
+                        <div class="col-2">
+                            <img class="img-thumbnail rounded w-100" style="object-fit: cover;" src="{{ asset('images/vendor/admin-lte/dist/AdminLTELogo.png') }}" />
+                        </div>
                         <div class="col">
-                            {{ $service->name }}
-                            <a class="btn btn-app">
-                                <span class="badge bg-teal">67</span>
-                                <i class="fas fa-inbox"></i> Orders
-                            </a>
+                            <h5>{{ $service->name }}
+                                <small><i class="fas fa-piggy-bank"></i> € {{ $service->discount }}<del></del></small>
+                                <span class="badge bg-black"><i class="fas fa-pound-sign"></i> {{ $service->price }}</span>
+                            </h5>
+                            <p>
+                                <a class="btn btn-app">
+                                    <i class="fas fa-stopwatch"></i> {{ $service->duration }} minute(s)
+                                </a>
+                                <a class="btn btn-app">
+                                    <span class="badge bg-teal">99+</span>
+                                    <i class="fas fa-calendar-check"></i> Bookings
+                                </a>
+                            </p>
                         </div>
                     </div>
                 </li>
